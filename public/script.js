7f47fcb6a4dcc586ec20cf5aa747b1f66288d642
ralesu <ailche949@gmail.com>
2024-06-27 14:39:17 -0700

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,22 @@
+document.getElementById("todo-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  const todoInput = document.getElementById("todo-input");
+  const todoText = todoInput.value.trim();
+  if (todoText !== "") {
+    addTodoItem(todoText);
+    todoInput.value = "";
+  }
+});
+function addTodoItem(text) {
+  const li = document.createElement('li');
+  li.textContent = text;
+  const removeButton = document.createElement('button');
+  const butn = document.getElementById("butn");
+  removeButton.textContent = 'Delete'
+  removeButton.addEventListener('click', function() {
+    alert("You are deleting a task.");
+    li.remove();
+  });
+  li.appendChild(removeButton);
+  document.getElementById("todo-form").appendChild(li);
+}
